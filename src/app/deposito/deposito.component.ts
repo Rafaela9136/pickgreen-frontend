ebf4378209738dd469773cdfced85223af142a26
Rafaela9136 <rafaela9136@gmail.com>
2017-10-10 17:30:11 -0300

    Fix on submit

--- src/app/deposito/deposito.component.ts
+++ src/app/deposito/deposito.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Deposit } from './deposit.model';
 import { CrudService } from '../_services/crud.service';
 import { Http } from '@angular/http';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-deposito',
@@ -13,14 +14,15 @@ export class DepositoComponent implements OnInit {
   deposit: Deposit = new Deposit();
   deposits: Deposit[] = [];
 
-  constructor(private crudService: CrudService, private http: Http) { }
+  constructor(private crudService: CrudService, private http: Http, private router: Router) { }
 
   onSubmit() {
     for (let i = 0; i < this.deposits.length; i++) {
-      if (this.deposits[i].code === this.deposit.code && this.deposits[i].weight === this.deposit.weight) {
+      if (this.deposits[i].id === this.deposit.id) {
         this.deposit.status = 'validado';
         this.http.put('http://pick-green-api.herokuapp.com/depositApi' + this.deposit._id, this.deposit).subscribe(response => {
           window.alert('Depósito confirmado!');
+          this.router.navigate(['/confirmar-deposito']);
         }, error => {
           window.alert(error);
         });
