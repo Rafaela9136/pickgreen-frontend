a7806f59955366c7d3e44ad1babf1914e4dda205
Rafaela9136 <rafaela9136@gmail.com>
2017-10-21 22:28:26 -0300

    changing id to code

--- src/app/deposito/deposito.component.ts
+++ src/app/deposito/deposito.component.ts
@@ -27,7 +27,7 @@ export class DepositoComponent implements OnInit {
          window.alert(this.deposit._id);
         this.depositExists = true;
         this.deposit.status = 'validado';
-        this.http.put('https://pick-green-api.herokuapp.com/depositApi/' + this.deposit._id, this.deposit).subscribe(response => {
+        this.http.put('https://pick-green-api.herokuapp.com/depositApi/' + this.deposit[i]._id, this.deposit).subscribe(response => {
           this.loading = false;
           this.router.navigate(['/confirmar-deposito']);
           return window.alert('Depósito confirmado!');
