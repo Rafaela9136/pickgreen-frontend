6130db80db4fbd179a1e24951001cccdffd436f5
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-01-17 17:32:48 -0200

    Adição de coluna de código na tabela de coletores

--- src/app/admin-panel/collectors/collectors.component.ts
+++ src/app/admin-panel/collectors/collectors.component.ts
@@ -26,6 +26,7 @@ export class CollectorsComponent implements OnInit {
       if (typeof collectorFromModal !== 'undefined') {
         this.loading = true;
         this.crudService.create(this.route, collectorFromModal).subscribe(response => {
+          collectorFromModal.zipcode = response.zipcode;
           this.collectors.push(collectorFromModal);
           this.loading = false;
         }, error => {
